90d4337dc7d2a9c6449dd236239d61dc43941ff9
Justy116 <giustinogragnaniello116@gmail.com>
2022-06-06 18:14:41 +0200

    add new js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 let inputBill = document.querySelector(".input__bill");
 let inputPeople = document.querySelector(".input__people");
-let customInput = document.queryCommandIndeterm("custom__input");
+let customInput = document.querySelector(".custom__input");
 let tipMountOut = document.querySelector(".tip-mount-out");
 let totalOut = document.querySelector(".total-out");
+let buttonReset = document.getElementById("reset-button");
+
 
 let boxTip = document.querySelectorAll(".preset")
-boxTip.forEach((element) => {
+  boxTip.forEach((element)=>{
     element.addEventListener("click", ()=>{
-        console.log(element)
-        calculatorBill(inputBill.value, inputPeople.value, totalOut)
+      calculatorBill(inputBill.value, inputPeople.value, totalOut)
     })
-});
+  })
 
 function calculatorBill(bill, people, total){
   let resultBill = parseFloat(bill) / parseFloat(people);
   total.innerHTML = resultBill.toFixed(2);
 }
-
-// function calculatorTip(){
-  
-// }
